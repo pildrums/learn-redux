19aee1c73a967e65a767135bf7bf7ce1286d5122
pildrums <there7788@gmail.com>
2023-01-17 21:38:53 +0900

    Learn subscribe

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const initialState = {
+  number: 0,
+};
+
 // Reducer 함수 = 유일하게 Store에 있는 Data를 modify하는 함수
 // 오직 하나의 Reducer 함수만 가지게 된다.
-const countReducer = (count = 0, action) => {
-  if (action.type === "PLUS") {
+const countReducer = (count = initialState, action) => {
+  if (action.type === "INCREASE") {
     return count + 1;
-  } else if (action.type === "MINUS") {
+  } else if (action.type === "DECREASE") {
     return count - 1;
   } else {
     return count;
@@ -18,10 +22,19 @@ const countReducer = (count = 0, action) => {
 
 const countStore = legacy_createStore(countReducer);
 
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "PLUS" });
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleIncrease = () => {
+  countStore.dispatch({ type: "INCREASE" });
+};
+
+const handleDecrease = () => {
+  countStore.dispatch({ type: "DECREASE" });
+};
+
+plus.addEventListener("click", handleIncrease);
+minus.addEventListener("click", handleDecrease);
