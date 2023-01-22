06c8d655a905d4b467891f13f7e08058ad144829
pildrums <there7788@gmail.com>
2023-01-23 04:33:23 +0900

    Complete setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
